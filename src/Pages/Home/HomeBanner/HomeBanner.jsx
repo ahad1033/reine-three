9a269d3bce464df1e83cc9ac187bf8b6c1761bf6
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-11-26 18:21:01 +0600

    change the banner part of the home page

--- src/Pages/Home/HomeBanner/HomeBanner.jsx
+++ src/Pages/Home/HomeBanner/HomeBanner.jsx
@@ -1,45 +1,79 @@
 import React from "react";
-import slideOne from "../../../assets/images/slider/slide-1.jpg";
-import slideTwo from "../../../assets/images/slider/slide-2.jpg";
-import slideThree from "../../../assets/images/slider/slide-3.jpg";
+import slideOne from "../../../assets/images/banner-img/slide-11.jpg";
+import slideTwo from "../../../assets/images/banner-img/slide-12.jpg";
+import slideThree from "../../../assets/images/banner-img/slide-13.jpg";
+import slideFour from "../../../assets/images/banner-img/slide-14.jpg";
 
 const HomeBanner = () => {
   return (
-    <div
-      id="carouselExampleFade"
-      className="carousel slide carousel-fade"
-      data-bs-ride="carousel"
-      style={{ height: '650px' }}
-    >
-      <div className="carousel-inner">
-        <div className="carousel-item active">
-          <img src={slideOne} className="d-block w-100 custom-style" alt="..."  style={{ height: '650px', objectFit: 'cover' }}/>
+    <div id="hero-3" class="hero-section">
+      <div class="container">
+        {/* <!-- HERO-3 TEXT --> */}
+        <div class="row">
+          <div class="col">
+            <div class="hero-3-txt">
+              {/* <!-- Title --> */}
+              <h2>Our touch will make your hair look at its best</h2>
+
+              {/* <!-- Button --> */}
+              <a href="#lnk-1" class="btn btn--black hover--tra-black">
+                View Salon Menu
+              </a>
+            </div>
+          </div>
         </div>
-        <div className="carousel-item">
-          <img src={slideTwo} className="d-block w-100 custom-style" alt="..."  style={{ height: '650px', objectFit: 'cover' }}/>
+        {/* <!-- END HERO-3 TEXT --> */}
+
+        {/* <!-- SLIDER --> */}
+
+        <div
+          id="carouselExampleFade"
+          className="carousel slide carousel-fade"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src={slideOne} className="d-block w-100" alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src={slideTwo} className="d-block w-100" alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src={slideThree} className="d-block w-100" alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src={slideFour} className="d-block w-100" alt="..." />
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleFade"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleFade"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
         </div>
-        <div className="carousel-item">
-          <img src={slideThree} className="d-block w-100 custom-style" alt="..."  style={{ height: '650px', objectFit: 'cover' }}/>
-        </div>
+
+        {/* <!-- END SLIDER --> */}
       </div>
-      <button
-        className="carousel-control-prev"
-        type="button"
-        data-bs-target="#carouselExampleFade"
-        data-bs-slide="prev"
-      >
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Previous</span>
-      </button>
-      <button
-        className="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselExampleFade"
-        data-bs-slide="next"
-      >
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Next</span>
-      </button>
+      {/* <!-- End container --> */}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import '../src/assets/CSS/lunar.css';
-import '../src/assets/CSS/magnific-popup.css';
-import '../src/assets/CSS/menu.css';
-import '../src/assets/CSS/flaticon.css';
-import '../src/assets/CSS/bootstrap.min.css';
-import '../src/assets/CSS/style.css';
-import '../src/assets/CSS/custom.css';
-import '../src/assets/CSS/animate.css';
-import '../src/assets/CSS/responsive.css';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './Routes/Routers';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import "../src/assets/CSS/lunar.css";
+import "../src/assets/CSS/magnific-popup.css";
+import "../src/assets/CSS/menu.css";
+import "../src/assets/CSS/flaticon.css";
+import "../src/assets/CSS/bootstrap.min.css";
+import "../src/assets/CSS/style.css";
+import "../src/assets/CSS/custom.css";
+import "../src/assets/CSS/animate.css";
+import "../src/assets/CSS/responsive.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Routers";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
